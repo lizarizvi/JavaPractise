a07cd12a639e0527571ee2037e0fa9feb098ebd8
liza <lizarizvi333@gmail.com>
2024-03-05 10:16:46 +0530

    Find Numbers with Even Number of Digits (leetcode)(condition if no of digits are negative)

--- src/com/array/EvenDigitsArray.java
+++ src/com/array/EvenDigitsArray.java
@@ -3,7 +3,7 @@ package com.array;
 //https://leetcode.com/problems/find-numbers-with-even-number-of-digits/description/
 public class EvenDigitsArray {
     public static void main(String[] args) {
-        int[] arr = {555,901,482,1771};
+        int[] arr = {555,901,482,1771,01};
         System.out.println(findNumbers(arr));
     }
     static int findNumbers(int[] arr) {
@@ -17,6 +17,11 @@ public class EvenDigitsArray {
     }
     static int numOfDigits(int n){ //count the no of digits
         int c = 0;
+
+        if(n < 0){     //condition if no of digits are negative
+            n *= -1;
+        }
+
         while(n > 0){
             c++;
             n /= 10;

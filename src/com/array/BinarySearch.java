ef855dd4cbc97a413d5cabe3206523be23f62d2b
liza <lizarizvi333@gmail.com>
2024-03-06 17:30:20 +0530

    order agnostic binary search

--- src/com/array/BinarySearch.java
+++ src/com/array/BinarySearch.java
@@ -2,19 +2,20 @@ package com.array;
 
 public class BinarySearch {
     public static void main(String[] args) {
-        int[] arr = {-23, -10, -2, 0, 4, 32, 43, 67};
-        System.out.println(binary(arr, -10));
+        int[] arr = {-23, -10, -2, 0, 4, 32, 43, 67};  //ascending
+        int[] arr2 = {67, 43, 4, 0, -2, -23};   //descending
+        System.out.println(orderAgnosticBS(arr2, -2));
     }
 
-    static int binary(int[] arr, int target) {
+    static int binary(int[] arr, int target) {  //already known ascending or descending
         int start = 0;
         int end = arr.length - 1;
         while(start <= end){
             int mid = (start + end) / 2;
             // int mid = start + (end - start) / 2;
-            if (target < arr[mid]){
+            if (target < arr[mid]){    //if (target > arr[mid]) for descending order
                 end = mid - 1;
-            } else if (target > arr[mid]) {
+            } else if (target > arr[mid]) {   //if (target < arr[mid]) for descending order
                 start = mid + 1;
             }
             else {
@@ -23,4 +24,39 @@ public class BinarySearch {
         }
         return -1;
     }
+
+    static int orderAgnosticBS(int[] arr, int target){
+        int start = 0;
+        int end = arr.length - 1;
+
+        // check whether ascending or descending
+        boolean isAsc = arr[start] < arr[end];
+
+        while(start <= end){
+            int mid = (start + end) / 2;
+            // int mid = start + (end - start) / 2;
+
+            if (arr[mid] == target){
+                return mid;
+            }
+
+            if (isAsc){
+                if (target < arr[mid]){    //if (target > arr[mid]) for descending order
+                    end = mid - 1;
+                } else if (target > arr[mid]) {   //if (target < arr[mid]) for descending order
+                    start = mid + 1;
+                }
+            }
+
+            else {
+                if (target > arr[mid]){    //if (target > arr[mid]) for descending order
+                    end = mid - 1;
+                } else if (target < arr[mid]) {   //if (target < arr[mid]) for descending order
+                    start = mid + 1;
+                }
+            }
+        }
+        return -1;
+    }
+
 }

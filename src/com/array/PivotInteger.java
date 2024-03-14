292ed31154c8723ac977600402bd2a1a8afe8ee1
liza <lizarizvi333@gmail.com>
2024-03-14 12:40:48 +0530

    Find the Pivot Integer-better solution (leetcode)

--- src/com/array/PivotInteger.java
+++ src/com/array/PivotInteger.java
@@ -5,20 +5,26 @@ public class PivotInteger {
         System.out.println(pivotInteger(8));
     }
     static int pivotInteger(int n) {
-        int mid = 1 + (n - 1) / 2;
-        for (int i = 1; i <= n; i++) {
-            int tillMidSum = (mid * (mid + 1)) / 2;
-            int tillEndSum = (n * (n + 1)) / 2;
-            int tillLeftMidSum = ((mid - 1) * (mid)) / 2;
-            int midEndSum = tillEndSum - tillLeftMidSum;
-            if (tillMidSum == midEndSum) {
-                return mid;
-            } else if (tillMidSum < midEndSum) {
-                mid++;
-            } else {
-                mid--;
-            }
-        }
-        return -1;
+//        int mid = 1 + (n - 1) / 2;
+//        for (int i = 1; i <= n; i++) {
+//            int tillMidSum = (mid * (mid + 1)) / 2;
+//            int tillEndSum = (n * (n + 1)) / 2;
+//            int tillLeftMidSum = ((mid - 1) * (mid)) / 2;
+//            int midEndSum = tillEndSum - tillLeftMidSum;
+//            if (tillMidSum == midEndSum) {
+//                return mid;
+//            } else if (tillMidSum < midEndSum) {
+//                mid++;
+//            } else {
+//                mid--;
+//            }
+//        }
+//        return -1;
+
+        //better solution with less complexity
+        int y = n*(n+1)/2;
+        int x = (int)Math.sqrt(y);
+        if(x*x==y) return x;
+        else return -1;
     }
 }

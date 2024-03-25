a5ac0165e7b5fbb6b028ffaacd73a16f9a87c392
liza <lizarizvi333@gmail.com>
2024-03-25 16:59:28 +0530

    kth missing positive integer (leetcode)

--- src/com/array/KthMissing.java
+++ src/com/array/KthMissing.java
@@ -2,38 +2,33 @@ package com.array;
 //https://leetcode.com/problems/kth-missing-positive-number/description/
 public class KthMissing {
     public static void main(String[] args) {
-        int[] arr = {2};
-        int k = 2;
+        int[] arr = {2,3,4,7,11};
+        int k = 12;
         System.out.println(findKthPositive(arr, k));
     }
     static int findKthPositive(int[] arr, int k) {
-        int[] arr2 = new int[arr.length * arr.length * 2];
-        int a = 0;
-            for (int i = 1; i < arr2.length; i++) {
-                if (!binary(arr, i)) {
-                    arr2[a] = i;
-                    a++;
-                    if (a >= k)
-                        break;
-                }
-            }
-        return arr2[k - 1];
-    }
-    static boolean binary(int[] arr, int target) {  //already known ascending or descending
+//        for (int i = 0; i < arr.length; i++) {
+//            int ans = arr[i];
+//            if (ans <= k) {
+//                k++;
+//            } else {
+//                break;
+//            }
+//        }
+//        return k;
+
         int start = 0;
         int end = arr.length - 1;
-        while(start <= end){
-            int mid = (start + end) / 2;
-            // int mid = start + (end - start) / 2;
-            if (target < arr[mid]){    //if (target > arr[mid]) for descending order
-                end = mid - 1;
-            } else if (target > arr[mid]) {   //if (target < arr[mid]) for descending order
+        while (start <= end) {
+            int mid = start + ((end - start) / 2);
+            int missing = arr[mid] - (mid + 1);
+            if (missing < k) {
                 start = mid + 1;
             }
             else {
-                return true;
+                end = mid - 1;
             }
         }
-        return false;
+        return start + k;
     }
 }

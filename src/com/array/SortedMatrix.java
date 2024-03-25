807c56fdb30607dca21fd4db2363f12ddb30e0ac
liza <lizarizvi333@gmail.com>
2024-03-25 20:46:57 +0530

    sorted matrix correction - only for matrix where no of col remain same in each row

--- src/com/array/SortedMatrix.java
+++ src/com/array/SortedMatrix.java
@@ -4,8 +4,8 @@ import java.util.Arrays;
 
 public class SortedMatrix {
     public static void main(String[] args) {
-        int[][] matrix = {{0,1,2,3},{4,5,6},{7,8,9},{10,11,12}};
-        int target = 9;
+        int[][] matrix = {{1,2,3},{4,5,6},{7,8,9},{10,11,12}};
+        int target = 12;
         System.out.println(Arrays.toString(search(matrix, target)));
     }
 

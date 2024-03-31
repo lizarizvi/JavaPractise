6f83c1340c3aef3479e8a57ed4ab33d18d42f074
liza <lizarizvi333@gmail.com>
2024-03-31 14:02:58 +0530

    Find the all the Duplicate Number (leetcode)

--- src/com/array/DuplicateNumber.java
+++ src/com/array/DuplicateNumber.java
@@ -1,9 +1,14 @@
 package com.array;
+
+import java.util.ArrayList;
+import java.util.List;
+
 //https://leetcode.com/problems/find-the-duplicate-number/description/
 public class DuplicateNumber {
     public static void main(String[] args) {
-        int[] arr = {1,3,4,2,2};
+        int[] arr = {4,3,2,7,8,2,3,1};
         System.out.println(findDuplicate(arr));
+        System.out.println(findAllDuplicates(arr));
     }
     static int findDuplicate(int[] arr) {
         int i = 0;
@@ -33,5 +38,40 @@ public class DuplicateNumber {
         int temp = arr[i];
         arr[i] = arr[i1];
         arr[i1] = temp;
+    }
+//https://leetcode.com/problems/find-all-duplicates-in-an-array/description/
+    static List<Integer> findAllDuplicates(int[] arr) {
+//        int i = 0;
+//        ArrayList<Integer> arrayList = new ArrayList<>();
+//        while (i < arr.length){
+//            if (arr[i] != i + 1){
+//                if (arr[i] != arr[arr[i] - 1]){
+//                    swap(arr, i, arr[i] - 1);
+//                } else {
+//                    arrayList.add(arr[i]);
+//                    i++;
+//                }
+//            } else {
+//                i++;
+//            }
+//        }
+//        Collections.sort(arrayList);
+//        return arrayList;
+        int i = 0;
+        while (i < arr.length) {
+            if (arr[i] != arr[arr[i] - 1]) {
+                swap(arr, i, arr[i] - 1);
+            } else {
+                i++;
+            }
+        }
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        for (int j = 0; j < arr.length; j++) {
+            if (arr[j] != j + 1) {
+                arrayList.add(arr[j]);
+            }
+        }
+
+        return arrayList;
     }
 }

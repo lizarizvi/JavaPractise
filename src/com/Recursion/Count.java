bd7b5d5f536cb36e80c7afd2ad9856230101bec2
liza <lizarizvi333@gmail.com>
2024-04-05 15:47:21 +0530

    count zeroes and count number steps (leetcode) using recursion

---
+++ src/com/Recursion/Count.java
@@ -0,0 +1,33 @@
+package com.Recursion;
+
+public class Count {
+    public static void main(String[] args) {
+        System.out.println(countZeroes(76500760, 0));
+        System.out.println(numberOfSteps(123));
+    }
+    static int countZeroes(int n, int c){
+        if (n == 0){
+            return c;
+        }
+        if (n % 10 == 0){
+            return countZeroes(n / 10,c + 1);
+        }
+        return countZeroes(n / 10, c);
+
+    }
+
+    //https://leetcode.com/problems/number-of-steps-to-reduce-a-number-to-zero/
+    static int numberOfSteps(int num) {
+        return helper(num, 0);
+    }
+    static int helper(int num, int steps){
+        if (num == 0){
+            return steps;
+        }
+        if (num % 2 == 0){
+            return helper(num / 2, steps + 1);
+        }
+        return helper(num - 1, steps + 1);
+    }
+
+}

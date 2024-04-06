5ff55a6922ac4df151a498093807af062ec0fb20
liza <lizarizvi333@gmail.com>
2024-04-06 17:24:20 +0530

    returning the list of index found using recursion

--- src/com/Recursion/SortedArray.java
+++ src/com/Recursion/SortedArray.java
@@ -1,9 +1,13 @@
 package com.Recursion;
 
+import java.util.ArrayList;
+
 public class SortedArray {
     public static void main(String[] args) {
         int[] arr = {1,2,2,6,7,9,12};
         System.out.println(linearSearch(arr,0, 5));
+        findAllIndex(arr, 0, 2);
+        System.out.println(arrayList);
     }
 
     static boolean sortedOrNot(int[] arr, int index) {
@@ -20,5 +24,16 @@ public class SortedArray {
             return index;
         }
         return linearSearch(arr, index + 1, target);
+    }
+
+    static ArrayList<Integer> arrayList = new ArrayList<>();
+    static void findAllIndex(int[] arr, int index, int target){
+        if (index == arr.length - 1){
+            return;
+        }
+        if (arr[index] == target) {
+            arrayList.add(index);
+        }
+        findAllIndex(arr, index + 1, target);
     }
 }

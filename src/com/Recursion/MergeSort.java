b2796cfea46dc012b2e75f3d4c8e931cf0a3ff94
liza <lizarizvi333@gmail.com>
2024-04-12 18:43:06 +0530

    merge sort (method 2) using recursion

--- src/com/Recursion/MergeSort.java
+++ src/com/Recursion/MergeSort.java
@@ -6,6 +6,9 @@ public class MergeSort {
     public static void main(String[] args) {
         int[] arr = {8,3,4,12,5,6};
         System.out.println(Arrays.toString(mergeSort(arr)));
+
+        mergeSortInPlace(arr, 0, arr.length);
+        System.out.println(Arrays.toString(arr));
     }
 
     static int[] mergeSort(int[] arr) {
@@ -53,5 +56,56 @@ public class MergeSort {
         }
 
         return mergedArray;
+    }
+
+
+    static void mergeSortInPlace(int[] arr, int start, int end) {
+        if (end - start == 1){ //base condition when only one element left
+            return;
+        }
+        int mid = (start + end) / 2;
+        //sorting first and second side of array
+        mergeSortInPlace(arr, start, mid);
+        mergeSortInPlace(arr, mid, end);
+
+        // merge function merge first and second array
+        mergeInPlace(arr, start, mid, end);
+    }
+
+    static void mergeInPlace(int[] arr, int start, int mid, int end) {
+        //new array that will contain merged and sorted array
+        int[] mergedArray = new int[end - start];
+        int i = start; //pointer for first[]
+        int j = mid; //pointer for second[]
+        int k = 0; //pointer for mergedArray[]
+        //while reaching one of its length loop will end and
+        //will check separately if element in first or second is left
+        while (i < mid && j < end){
+            if (arr[i] < arr[j]){
+                mergedArray[k] = arr[i];
+                i++;
+            } else {
+                mergedArray[k] = arr[j];
+                j++;
+            }
+            k++;
+        }
+        //loop will work when there are elements left in first[]
+        while (i < mid){
+            mergedArray[k] = arr[i];
+            i++;
+            k++;
+        }
+        //loop will work when there are elements left in second[]
+        while (j < end){
+            mergedArray[k] = arr[j];
+            j++;
+            k++;
+        }
+
+        System.arraycopy(mergedArray, 0, arr, start, mergedArray.length);
+
+        // System.arraycopy(mergedArray, 0, arr, start + 0, mergedArray.length);
+
     }
 }

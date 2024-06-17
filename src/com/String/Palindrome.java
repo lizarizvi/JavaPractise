e8f8e986213c4205877f79ef95a30d9504225644
liza <lizarizvi333@gmail.com>
2024-06-17 20:16:47 +0530

    Palindrome with StringBuilder

--- src/com/String/Palindrome.java
+++ src/com/String/Palindrome.java
@@ -2,24 +2,33 @@ package com.String;
 
 public class Palindrome {
     public static void main(String[] args) {
-        String str = "abcdedcba";
-        System.out.println(isPalindrome(str));
+        String str = "abcdcba";
+        System.out.println(isPalindrome2(str));
     }
-    static boolean isPalindrome(String str){
+//    static boolean isPalindrome(String str){
+//        if (str == null){
+//            return true;
+//        }
+//        str = str.toLowerCase();
+//        int start = 0;
+//        int end = str.length() - 1;
+//        while(start <= end){
+//            if (str.charAt(start) != str.charAt(end)){
+//                return false;
+//            } else {
+//                start++;
+//                end--;
+//            }
+//        }
+//        return true;
+//    }
+
+    static boolean isPalindrome2(String str){
         if (str == null){
             return true;
         }
-        str = str.toLowerCase();
-        int start = 0;
-        int end = str.length() - 1;
-        while(start <= end){
-            if (str.charAt(start) != str.charAt(end)){
-                return false;
-            } else {
-                start++;
-                end--;
-            }
-        }
-        return true;
+        StringBuilder builder = new StringBuilder(str);
+        String reverse = builder.reverse().toString();
+        return str.equals(reverse);
     }
 }

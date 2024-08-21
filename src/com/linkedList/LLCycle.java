9e135d1ad57454e673ca6b8c620251867568056f
liza <lizarizvi333@gmail.com>
2024-08-21 19:39:11 +0530

    node where the linked list cycle begins (leetcode)

--- src/com/linkedList/LLCycle.java
+++ src/com/linkedList/LLCycle.java
@@ -41,4 +41,35 @@ public class LLCycle {
         }
         return 0;
     }
+
+    //https://leetcode.com/problems/linked-list-cycle-ii/description/
+    public ListNode detectCycle(ListNode head) {
+        int length = 0;
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast != null && fast.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow){
+                length = lengthCycle(slow);
+                break;
+            }
+        }
+
+        if (length == 0){
+            return null;
+        }
+
+        ListNode first = head;
+        ListNode second = head;
+        while (length > 0){
+            second = second.next;
+            length--;
+        }
+        while (first != second){
+            first = first.next;
+            second = second.next;
+        }
+        return second;
+    }
 }

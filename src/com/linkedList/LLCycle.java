e994d6c176e37de15fe8de232b641a2619f2d479
liza <lizarizvi333@gmail.com>
2024-08-21 17:26:48 +0530

    linked list cycle (leetcode)

---
+++ src/com/linkedList/LLCycle.java
@@ -0,0 +1,24 @@
+package com.linkedList;
+//https://leetcode.com/problems/linked-list-cycle/
+public class LLCycle {
+    class ListNode {
+      int val;
+      ListNode next;
+      ListNode(int x) {
+          val = x;
+          next = null;
+      }
+    }
+    public boolean hasCycle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast != null && fast.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow){
+                return true;
+            }
+        }
+        return false;
+    }
+}

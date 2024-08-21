6e445c6d48fc0d886d6f371f2510f6c44e59f4b4
liza <lizarizvi333@gmail.com>
2024-08-21 17:58:04 +0530

    length of linked list cycle (leetcode)

--- src/com/linkedList/LLCycle.java
+++ src/com/linkedList/LLCycle.java
@@ -21,4 +21,24 @@ public class LLCycle {
         }
         return false;
     }
+
+    //length of the cycle
+    public int lengthCycle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast != null && fast.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow){
+                ListNode node = slow;
+                int length = 0;
+                do{
+                    node = node.next;
+                    length++;
+                } while (node != fast);
+                return length;
+            }
+        }
+        return 0;
+    }
 }
